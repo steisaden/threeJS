76ab69594095d7ef23b3ae44d1eb3f5fd90cb4fe
Steisaden <apple25@stephens-MacBook-Pro.local>
2023-04-10 13:43:51 -0700

    first commit

---
+++ server/routes/dalle.routes.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import * as dotenv from 'dotenv'
+dotenv.config();
+import { Configuration, OpenAIApi } from 'openai';
+
+const router = express.Router();
+const configuration = new Configuration({
+  apiKey: process.env.REACT_APP_OPENAI_API,
+});
+
+const openai = new OpenAIApi(configuration);
+
+router.route('/').get((req, res) => {
+  res.status(200).json({ message: "Hello from DALL.E ROUTES" })
+})
+
+router.route('/').post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+  
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'b64_json',
+    });
+
+    const image = response.data.data[0].b64_json;
+
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" })
+  }
+})
+
+export default router;
