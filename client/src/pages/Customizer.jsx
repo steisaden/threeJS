1fd965cef24933a21201bc4c437e066fa1716e78
Steisaden <apple25@stephens-MacBook-Pro.local>
2023-04-10 16:25:45 -0700

    final code6

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useSnapshot } from 'valtio';
-
-import config from '../config/config';
+import * as dotenv from 'dotenv';
+dotenv.config();
 import state from '../store';
 import { download } from '../assets';
 import { downloadCanvasToImage, reader } from '../config/helpers';
